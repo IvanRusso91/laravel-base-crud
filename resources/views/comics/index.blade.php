945b423325752ad1dd928fa971274154c00b045c
IvanRusso91 <iv.russo91@gmail.com>
2022-06-29 18:17:20 +0200

    prima parte inserita

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,28 +1,31 @@
-@include('layout.main')
+@extends('layout.main')
 
 @section('content')
 
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">title</th>
-        <th scope="col">Type</th>
-      </tr>
-    </thead>
-    <tbody>
-        @foreach ($comics as $comic)
-        <tr>
-          <th scope="row">{{$comic->id}}</th>
-          <td>{{$comic->title}}</td>
-          <td>{{$comic->type}}</td>
-          <td>
-            <a class="btn btn-success" href="{{route('comics.show',$comic)}}">Show</a>
-            <a class="btn btn-primary" href="">EDIT</a>
-          </td>
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
+    <div class="container box-ir">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">title</th>
+                <th scope="col">Type</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                <tr>
+                  <th scope="row">{{$comic->id}}</th>
+                  <td>{{$comic->title}}</td>
+                  <td>{{$comic->type}}</td>
+                  <td>
+                    <a class="btn btn-success" href="{{route('comics.show',$comic)}}">Show</a>
+                    <a class="btn btn-primary" href="">EDIT</a>
+                  </td>
+                </tr>
+                @endforeach
+            </tbody>
+          </table>
+
+    </div>
 
 @endsection
