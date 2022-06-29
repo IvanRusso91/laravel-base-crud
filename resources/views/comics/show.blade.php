db747f32d8b96822b50d7c2eedc3fe55b1b8a2e0
IvanRusso91 <iv.russo91@gmail.com>
2022-06-29 18:43:09 +0200

    completato e decorato

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,5 +2,17 @@
 
 @section('content')
 
+    <div class="container text-center ">
+        <h1 class="titolo">{{$comic->title}} <a class="btn btn-primary" href="">EDIT</a></h1>
+        <span class="bg-info badge text-bg-info">{{$comic->type}}</span>
+
+            <div class="img-box">
+                <img src="{{$comic->image}}" alt="{{$comic->title}}">
+            </div>
+
+            <a class="btn btn-secondary" href="{{ route('comics.index')}}"><< Torna</a>
+
+        </div>
+    </div>
 
 @endsection
