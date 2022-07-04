98ba3ef134213bb541cd1ed99d9eb5b2e3b86241
IvanRusso91 <iv.russo91@gmail.com>
2022-07-04 11:25:46 +0200

    aggiunti controlli in edit e create

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -36,6 +36,33 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
+
+        $request->validate(
+            [
+              'title' => 'required | max:50 | min:3',
+              'image' => 'required | max:255 | min:10',
+              'type' => 'required | max:50 | min:3',
+              'description' => 'min:10'
+            ],
+            [
+                'title.required'=> 'il campo title è obbligatorio',
+                'title.max'=> 'il campo title deve avere al amssimo :max carratteri',
+                'title.max'=> 'il campo title deve avere al amssimo :min carratteri',
+
+                'image.required'=> 'il campo Url è obbligatorio',
+                'image.max'=> 'il campo Url deve avere al amssimo :max carratteri',
+                'image.max'=> 'il campo Url deve avere al amssimo :min carratteri',
+
+                'type.required'=> 'il campo type è obbligatorio',
+                'type.max'=> 'il campo type deve avere al amssimo :max carratteri',
+                'type.max'=> 'il campo type deve avere al amssimo :min carratteri',
+
+                'description.min' => 'la descrizione deve avere minimo :min caratteri'
+
+
+            ],
+        );
+
         $data = $request-> all();
         $new_comic= new Comic();
         $data['slug'] = Str::slug($data['title'], '-');
@@ -86,6 +113,32 @@ class ComicController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
+
+        $request->validate(
+            [
+              'title' => 'required | max:50 | min:3',
+              'image' => 'required | max:255 | min:10',
+              'type' => 'required | max:50 | min:3',
+              'description' => 'min:10'
+            ],
+            [
+                'title.required'=> 'il campo title è obbligatorio',
+                'title.max'=> 'il campo title deve avere al amssimo :max carratteri',
+                'title.max'=> 'il campo title deve avere al amssimo :min carratteri',
+
+                'image.required'=> 'il campo Url è obbligatorio',
+                'image.max'=> 'il campo Url deve avere al amssimo :max carratteri',
+                'image.max'=> 'il campo Url deve avere al amssimo :min carratteri',
+
+                'type.required'=> 'il campo type è obbligatorio',
+                'type.max'=> 'il campo type deve avere al amssimo :max carratteri',
+                'type.max'=> 'il campo type deve avere al amssimo :min carratteri',
+
+                'description.min' => 'la descrizione deve avere minimo :min caratteri'
+
+
+            ],
+        );
 
         $data= $request->all();
         $data['slug']=Str::slug($data['title'], '-');
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,6 +3,19 @@
 @section('content')
 
 <div class="container my-5">
+
+    <div class="row">
+        @if ($errors->any())
+            <div class="col-8 offset-2 alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error )
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
+
     <div class="row">
         <div class="col-8 offset-2">
             <h2 class="mb-3">Inserisci una nuovo comics</h2>
@@ -11,19 +24,39 @@
                 @csrf
                 <div class="mb-3">
                     <label for="title" class="form-label">Comics Title</label>
-                    <input type="text" id="title" name="title" class="form-control" placeholder="Comics Title">
+                    <input type="text" id="title" name="title" class="form-control @error('title') is-invalid @enderror" placeholder="Comics Title" value="{{ old('title')}}">
+                    @error('title')
+                        <p class="form-control">
+                            {{$message}}
+                        </p>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="type" class="form-label">Tipo</label>
-                    <input type="text" id="type" name="type" class="form-control" placeholder="Comics Type" >
+                    <input type="text" id="type" name="type" class="form-control @error('type') is-invalid @enderror" placeholder="Comics Type"  value="{{ old('type')}}">
+                    @error('type')
+                        <p class="form-control">
+                            {{$message}}
+                        </p>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="image" class="form-label">URL immage</label>
-                    <input type="text" id="image" name="image" class="form-control" placeholder="URL immagine" >
+                    <input type="text" id="image" name="image" class="form-control @error('image') is-invalid @enderror" placeholder="URL immagine" value="{{ old('image')}}">
+                    @error('image')
+                        <p class="form-control">
+                            {{$message}}
+                        </p>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="description" class="form-label">description</label>
-                    <textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
+                    <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" cols="30" rows="10" value="{{ old('description')}}"></textarea>
+                    @error('description')
+                        <p class="form-control">
+                            {{$message}}
+                        </p>
+                    @enderror
                 </div>
 
                 <button type="submit" class="btn btn-primary">Invia</button>
